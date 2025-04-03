05b1a65266b4bdab97e44c228986eee84ee23d22
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-03 18:35:48 -0300

    no refleaja cambios en breadcrumbs

--- src/app/configuracion/clientes/agregar/page.tsx
+++ src/app/configuracion/clientes/agregar/page.tsx
@@ -9,10 +9,10 @@ export default async function Page() {
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: 'Clientes', href: '/configuracion/clientes' },
           {
-            label: 'Create Invoice',
-            href: '/dashboard/invoices/create',
+            label: 'Agregar cliente',
+            href: '/configuracion/clientes/agregar',
             active: true,
           },
         ]}
