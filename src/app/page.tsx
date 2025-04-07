326ff440972b4f60bb32db914398a5c0966c24bd
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-07 18:11:58 -0300

    cambios diseño pagina principal v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
     <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col">
         <Image
-          className="dark:invert"
+          className="dark:invert w-400 h-400"
           src="/logonuevo.svg"
           alt="Move logo"
-          width={400}
-          height={400}
           priority
         />
         <h1 className="text-4xl sm:text-5xl  text-center">
@@ -18,16 +16,13 @@ export default function Home() {
         <p className="text-center text-lg sm:text-xl">
           Aplicacion de reservas de turnos
         </p>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+        <a
             className="rounded-full border border-solid border-[#01feab] dark:border-white/[.145] transition-colors flex items-center justify-center bg-[#212121] hover:bg-[#00885b] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="/perfil"
             rel="noopener noreferrer"
           >
             Iniciar Sesion
-          </a>
-        </div>
+        </a>
       </main>
     </div>
   );
