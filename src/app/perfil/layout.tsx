0a51a8aff848d9e65146909e47c559ffe793d132
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-08 00:30:26 -0300

    eliminar el flex none del div layout

--- src/app/perfil/layout.tsx
+++ src/app/perfil/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from "@/app/ui/perfil/sidenav";
 export default function Layout({  children,}: Readonly<{  children: React.ReactNode;}>) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="w-full md:w-64">
         <SideNav />
       </div>
       <div className="grow p-6 md:overflow-y-auto md:p-12">
