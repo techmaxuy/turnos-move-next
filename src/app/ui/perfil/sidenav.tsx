0cd33fe2769c1368c9f71da01050aa19476fc4a6
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-09 16:42:43 -0300

    cambio en pagina perfil

--- src/app/ui/perfil/sidenav.tsx
+++ src/app/ui/perfil/sidenav.tsx
@@ -5,22 +5,22 @@ import Image from "next/image";
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className=""
-        href="/"
-      >
         <div className="">
-          <Image
-                    className="dark:invert"
-                    src="/logonuevo.svg"
-                    alt="Move logo"
-                    width={150}
-                    height={150}
-                    priority
-                  />
-        </div>
-      </Link>
+          
       <div className="flex grow flex-row justify-between space-x-1 md:flex-col md:space-x-0 md:space-y-1">
+      <Link
+           className=""
+            href="/"
+          > 
+            <Image
+              className="dark:invert"
+              src="/logonuevo.svg"
+              alt="Move logo"
+              width={150}
+              height={150}
+              priority
+            />
+          </Link>
         <NavLinks />
         <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-[#212121] p-3 text-xs font-small md:text-sm md:font-medium hover:bg-[#01feab] hover:text-black md:flex-none md:justify-start md:p-2 md:px-3">       
@@ -28,6 +28,9 @@ export default function SideNav() {
           </button>
         </form>
       </div>
+    </div>
+
+
     </div>
   );
 }
