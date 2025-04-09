bd9f8d3decf049e6531634b4c939cd29fd13ea80
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-09 18:14:25 -0300

    texto error

--- src/app/ui/configuracion/sidenavconf.tsx
+++ src/app/ui/configuracion/sidenavconf.tsx
@@ -23,7 +23,7 @@ export default function SideNav() {
         <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-[#212121] p-3 text-xs font-small md:text-sm md:font-medium hover:bg-[#01feab] hover:text-black md:flex-none md:justify-start md:p-2 md:px-3">Cerrar Sesion
           </button>
-        </form>http://localhost:3000/configuracion/clientes/agregar
+        </form>
       </div>
     </div>
   );
