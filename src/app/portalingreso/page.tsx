9843df95310e5639ca55dcc64821364a30449225
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-23 20:58:19 -0300

    add suspense en portal ingreso

--- src/app/portalingreso/page.tsx
+++ src/app/portalingreso/page.tsx
@@ -1,10 +1,14 @@
 import FormIngreso from "@/app/ui/portalingreso/formIngreso";
 import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
 export default function IngresoPage() {
   return (
     <div className="mx-2 mb-4 md:mt-15">
       <h1 className={`${lusitana.className} text-2xl p-x-1 text-center`}>Ingreso</h1>
+      <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
+
       <FormIngreso  />
+        </Suspense>
     </div>
   );
 }  
