57965c2fcfb06a6b40b3d27bd50c139c50e57feb
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-25 18:41:00 -0300

    publicacion mobil

--- src/app/portalingreso/page.tsx
+++ src/app/portalingreso/page.tsx
@@ -17,22 +17,16 @@ export default async function Page(props: {
 
   return (
     <div className="mx-2 mb-4 md:mt-15">
-      <h1 className={`${lusitana.className} text-2xl p-x-1 text-center`}>Ingreso</h1>
+      <h1 className={`${lusitana.className} text-2xl px-1 py-2 text-center`}>Portal de Ingreso</h1>
         <div className="flex flex-col w-full rounded-xl bg-[#568072] p-4 md:pt-0 mt-2 ">
-                <h2 className={`${lusitana.className} text-xl p-4 text-center`}>
-               Identificacion (CI , DNI)
-                </h2>
                 <div className="flex flex-col items-center pb-2 pt-6">
                     <Suspense fallback={null}>
-                        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                        <div className="flex items-center w-1/3 justify-between gap-2 md:mt-8">
+                          <label className="text-sm text-white">CI</label>
                             <Search placeholder="identificacion..." />
                         </div>
-                        
                     </Suspense>
                     <CustomerTable query={query}/>
-                    <div className="mt-5 flex w-full justify-center">
-                    {/* <Pagination totalPages={totalPages} /> */}
-                    </div>
                     <div>
                         <h3 className="ml-2 text-sm text-white ">Actualizado justo ahora</h3>
                     </div>
--- src/app/ui/portalingreso/table.tsx
+++ src/app/ui/portalingreso/table.tsx
@@ -1,4 +1,4 @@
-
+import clsx from 'clsx';
 import Image from 'next/image';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 
@@ -12,13 +12,20 @@ export default async function CustomerTable({
   const customers = await fetchFilteredCustomers(query);
 
   return (
-    <div className="flex flex-col w-full rounded-xl bg-[#568072] p-4 md:pt-0 mt-2 ">
-          <div className="flex flex-col justify-between py-2 text-center text-sm font-medium text-black bg-white rounded-lg">
+    <div className="flex flex-col w-full rounded-xl bg-[#568072] p-4 md:pt-0 mt-2 min-h-120">
+          <div className="flex flex-col justify-between py-2 text-center text-sm font-medium text-black bg-white rounded-lg min-h-100 place-items-center">
             {customers?.map((invoice) => (
               <>
-                <p className="text-4xl py-4">{invoice.name}</p>
-                <p>{invoice.ci}</p>
-                <p>{invoice.creditos}</p>
+                <p 
+                  className={clsx(
+                  "py-10 my-5",{"hidden" : !(invoice.name)}
+                  )}>
+                    <p className="text-2xl">Bienvenido !! </p>
+                    <p className="text-4xl ">{invoice.name}</p>
+                    <p>{invoice.creditos}</p>
+                    
+                </p>
+                
               </>
             ))}
           </div>
