d279bb19bc1dee5aa4d1e4bc23625c95614e0bfe
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-25 17:22:02 -0300

    nombre 4xl

--- src/app/ui/portalingreso/table.tsx
+++ src/app/ui/portalingreso/table.tsx
@@ -16,7 +16,7 @@ export default async function CustomerTable({
           <div className="flex flex-col justify-between py-2 text-center text-sm font-medium text-black bg-white rounded-lg">
             {customers?.map((invoice) => (
               <>
-                <p>{invoice.name}</p>
+                <p className="text-4xl">{invoice.name}</p>
                 <p>{invoice.ci}</p>
                 <p>{invoice.creditos}</p>
               </>
