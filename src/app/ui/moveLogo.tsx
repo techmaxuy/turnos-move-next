bce9cade7420c758266190eb53ab81250f69f524
Maximiliano DUva <techmax@adinet.com.uy>
2025-04-30 15:53:54 -0300

    pagina de login agregada

---
+++ src/app/ui/moveLogo.tsx
@@ -0,0 +1,13 @@
+
+import { lusitana } from '@/app/ui/fonts';
+
+export default function MoveLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+
+      <p className="text-[44px] ">Move</p>
+    </div>
+  );
+}
