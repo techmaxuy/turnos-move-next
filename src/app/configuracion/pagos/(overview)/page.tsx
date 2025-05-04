465c1ec7c0407e1d2e9f42a01d20933aabd8b2f5
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-04 19:57:52 -0300

    pagos protegido

--- src/app/configuracion/pagos/(overview)/page.tsx
+++ src/app/configuracion/pagos/(overview)/page.tsx
@@ -1,7 +1,16 @@
 import LatestPagos from "@/app/ui/configuracion/lastest-pagos";
 import { lusitana } from '@/app/ui/fonts';
 import { AgregarPago } from "@/app/ui/configuracion/pagos/buttons";
-export default function Configuracion() {
+import { auth } from "../../../auth"
+import NoAutenticado from "../../../ui/noAutenticado";
+
+
+export default async function Configuracion() {
+
+      const session = await auth()
+    if (!session) return <NoAutenticado />
+
+
   return (
     <div className="mx-2 mb-4 md:mt-15">
       <h1 className={`${lusitana.className} text-2xl p-x-1`}>Pagos</h1>
--- src/app/configuracion/pagos/agregar/page.tsx
+++ src/app/configuracion/pagos/agregar/page.tsx
@@ -1,8 +1,15 @@
 import Form from '@/app/ui/configuracion/pagos/create-form';
 import Breadcrumbs from '@/app/ui/configuracion/pagos/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
+import { auth } from "../../../auth"
+import NoAutenticado from "../../../ui/noAutenticado";
  
 export default async function Page() {
+
+     const session = await auth()
+    if (!session) return <NoAutenticado />
+
+
   const customers = await fetchCustomers();
  
   return (
