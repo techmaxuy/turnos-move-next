33fae23abd3f6acd362db5710da4af4908a9b82c
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-04 17:49:58 -0300

    console log dentro del autconfig

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -6,6 +6,9 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log('Auth:', auth);
+
+      
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/perfil');
       if (isOnDashboard) {
