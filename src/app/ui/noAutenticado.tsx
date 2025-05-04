7399a5058969e0bd94f84a5a1fe66c113c47aa84
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-04 19:46:55 -0300

    no autenticado componente add button

--- src/app/ui/noAutenticado.tsx
+++ src/app/ui/noAutenticado.tsx
@@ -3,6 +3,11 @@ export default function NoAutenticado() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold">No Autenticado</h1>
       <p className="mt-4">Por favor, inicia sesión para acceder a esta página.</p>
+      <a className="rounded-full border border-solid border-[#01feab] transition-colors flex items-center justify-center bg-[#212121] hover:bg-[#00885b] h-10 w-40"
+            href="/login"
+            rel="noopener noreferrer">
+            Iniciar Sesion
+      </a>
     </div>
   );
 }
