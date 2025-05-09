02ab86d0b2fa9f7d404841d2325d465413510788
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-08 21:29:10 -0300

    resend con domain portal21.work

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const result = await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'turnosmove@portal21.work',
     to: 'techmax@adinet.com.uy',
     subject: 'Hello World',
     html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
