85d7c0584a9a10953fb3187b7ba474489b471835
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-21 18:19:40 -0300

    principal 100dvh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body 
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-100dvh`}
       >
         {children}
         <MyFooter />
