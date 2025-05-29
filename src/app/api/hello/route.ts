3c26d117af4d58591e442587633982ad16a587e8
Maximiliano DUva <techmax@adinet.com.uy>
2025-05-29 11:44:28 -0300

    cambio en rute.ts en api hello

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -5,23 +5,16 @@ import { auth } from "../../auth"
 
 export  function GET(request: Request) {
 
-
-
-
   // Voy a crear una reserva de ejemplo para probar la api y para dejar un registro de la invocación
  
  const reserva = {
     clase: 'test cron job',
     hora: '5',
-    utilizada: 'false',
-    date: new Date().toISOString(),
   };
   
   const formData = new FormData();
 formData.append('clase', reserva.clase);
 formData.append('hora', reserva.hora);
-formData.append('utilizada', reserva.utilizada);
-formData.append('date', reserva.date);
 
 createReserva({}, formData);
     
