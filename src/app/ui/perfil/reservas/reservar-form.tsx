352a1ff948d62af1344c08236b3d997a80a11ccc
Maximiliano DUva <techmax@adinet.com.uy>
2025-06-04 10:01:38 -0300

    reserva final

--- src/app/ui/perfil/reservas/reservar-form.tsx
+++ src/app/ui/perfil/reservas/reservar-form.tsx
@@ -41,100 +41,22 @@ import { useState } from 'react';
             datosDisponibles.map((clase) => (
               clase.horas.map((hora) => {
                 return (
-                  <div key={hora} className="rounded-lg border-2 border-[#01feab]  text-sm text-white font-semibold mb-2 p-2 items-center text-center">
+                  <div key={hora} className="flex flex-row rounded-lg border-2 border-[#01feab]  text-sm text-white font-semibold mb-2 p-2 items-center text-center justify-between">
                     <p>{clase.nombre} - {hora} hs.</p>
+
+                      <Button type="submit">Reservar</Button>
+
                   </div>
                 ); 
               })
             ))
           }
 
-          {/* Clase */}
-          <div className="mb-4">
-            <label htmlFor="clase" className="mb-2 block text-sm font-medium">
-            Clase
-            </label>
-            <div className="relative">
-              <select
-                id="clase"
-                name="clase"
-                className="peer block w-full cursor-pointer rounded-md bg-[#757575] border border-[#757575] py-2 pl-10 text-sm outline-2 placeholder:text-black"
-                aria-describedby="clase-error"
-                onChange={handleSelectChange}
-                value={selectedValue}
-              >
-                <option className="text-black" value="" disabled>
-                  Seleccionar clase a reservar
-                </option>
-                {datosDisponibles.map((clase) => (
-                  <option  key={clase.clase_id} value={clase.nombre}>
-                    {clase.nombre}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div id="clase-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.clase &&
-                state.errors.clase.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
-            </div>
-          </div>
-
-          {/* Hora */}
-          <div className="mb-4">
-            <label htmlFor="hora" className="mb-2 block text-sm font-medium">
-            Hora
-            </label>
-            <div className="relative mt-2 rounded-md">
-              <div className="relative">
-                  <select
-                  id="hora"
-                  name="hora"
-                  className="peer block w-full cursor-pointer rounded-md bg-[#757575] border border-[#757575] py-2 pl-10 text-sm outline-2 placeholder:text-black"
-                  defaultValue=""
-                  aria-describedby="hora-error"
-                  >
-                  <option className="text-black" value="" disabled>
-                      Seleccionar hora a reservar
-                  </option>
-                  {datosDisponibles[0]?.horas.map((hora) => (
-                      <option  key={hora} value={hora}>
-                      {hora}
-                      </option>
-                  ))}
-                  </select>
-              </div>
-            </div>
-
-            <div id="hora-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.hora &&
-                state.errors.hora.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
-            </div>
-          </div>
-
-
           <div aria-live="polite" aria-atomic="true">
             {state.message ? (
               <p className="mt-2 text-sm text-red-500">{state.message}</p>
             ) : null}
           </div>
-        </div>
-        <div className="mt-6 flex justify-end gap-4">
-          <Link
-            href="/perfil/misreservas"
-            className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-          >
-            Cancel
-          </Link>
-          <Button type="submit">Reservar</Button>
         </div>
       </form>
 
